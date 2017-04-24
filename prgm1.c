76204d7e9e7af77a0ebf0680f0059a03440019a1
suryasadhana <suryasadhana74@gmail.com>
2017-04-24 11:00:09 +0530

    Create prgm1.c

---
+++ prgm1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int n;
+if(n>0)
+{
+printf("n is positive");
+}
+elseif(n<0)
+{
+printf("n is negative");
+}
+else
+{
+printf("n is zero");
+}
+getch();
+}
